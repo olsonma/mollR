6143e3cc053a999f10f92de9325701da694ea0c0
Molly Olson <mollyolson@Mollys-MacBook-Pro.local>
2022-06-02 16:10:53 -0400

    add keyword

--- R/compareLists.R
+++ R/compareLists.R
@@ -1,19 +1,20 @@
 #' Compile missing data summary of data set
-#' 
-#' Compares lists element-wise. Created for purpose of comparing two lists consisting of data frames, where each list is from a different data-pull date. 
+#'
+#' Compares lists element-wise. Created for purpose of comparing two lists consisting of data frames, where each list is from a different data-pull date.
 #' @param list1 a list
 #' @param list2 a list
 #' @param lnames a vector of length 2 supplying names of list
 #' @details The output is a text summary of element-wise list comparisons
-#' @keywords 
+#' @keywords list
 #' @export
-#' @examples 
+#' @examples
 #' l1 <- list(tmp1 = data.frame(x = 1:100, y = 100:1), tmp2 = data.frame(a = letters, b = LETTERS))
 #' l2 <- list(tmp1 = data.frame(x = 1:99, y = 99:1), tmp2 = data.frame(a = letters[1:14], b = LETTERS[1:14]))
 #' compareLists(l1, l2)
 #' compareLists(l2, l1)
 
-compareLists <- function(list1, list2, lnames){
+compareLists <- function(list1, list2, lnames)
+{
   if(missing(list1) | missing(list2)) stop("two lists must be specified")
   if(!is.list(list1) | !is.list(list2)) stop("input must be of type list")
   if(!missing(lnames)){ if(length(lnames) < 2) stop("names must be of length 2")}
