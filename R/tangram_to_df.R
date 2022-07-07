454db9d40485688e89440c14217cc0237c468f8c
Molly LaBelle <mao4003@med.cornell.edu>
2022-07-07 17:10:47 -0400

    add hide_p feature

--- R/tangram_to_df.R
+++ R/tangram_to_df.R
@@ -4,6 +4,7 @@
 #' @param tbl a `tangram` object (table)
 #' @param add_rown logical. Whether to add a row id (`id`) to the output dataframe
 #' @param html logical. Whether to include html code for indentation.
+#' @param hide_p logical. Whether to hide p-values for variables with "nopv" in the label (first column). This will remove the "nopv" text as well.
 #' @export
 #' @examples
 #' library(tangram)
@@ -12,8 +13,17 @@
 #'               style = "lancet",
 #'               id = "mytbl3")
 #' tangram_to_df(tbl = tab, html = F)
+#' 
+#' ## hide_p example
+#' tab2 <- tangram(drug~bili+albumin+stage::Categorical+protime+sex+age+spiders,
+#'               data = pbc %>% 
+#'               labelled::set_variable_labels(albumin = "nopv Albumin (gm/dl)"),
+#'               style = "lancet",
+#'               test = T,
+#'               id = "mytbl3")
+#' tangram_to_df(tbl = tab, html = F)
 
-tangram_to_df <- function(tbl, add_rown = FALSE, html = TRUE){
+tangram_to_df <- function(tbl, add_rown = FALSE, html = TRUE, hide_p = FALSE){
   if(missing(tbl)) stop("must provide argument tbl")
   if(all(class(tbl) != "tangram")) stop("tbl must be a tangram object")
   if(html){
@@ -30,6 +40,13 @@ tangram_to_df <- function(tbl, add_rown = FALSE, html = TRUE){
     nrow=length(tbl_list), 
     byrow=TRUE)) 
   
+  if(hide_p){
+    pcol <- which(tbl_df[1,] == "Test Statistic")
+    if(length(pcol) > 0){
+      tbl_df[[paste0("X",pcol)]] <- ifelse(grepl("nopv", tbl_df$X1), "", as.character(tbl_df[[paste0("X",pcol)]]))
+      tbl_df$X1 <- gsub("nopv", "", tbl_df$X1) %>% trimws
+    }
+  }
   if(add_rown){
     tbl_df <- tbl_df %>%
       `names<-`(c("Variables", "N", tbl_df[1,][3:ncol(.)])) %>%
